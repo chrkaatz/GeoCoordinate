0b2901b822f11dcdcf746fb02492a686a2a0edf1
Michał Wadas <michalwadas@gmail.com>
2016-11-30 19:00:04 +0100

    Refactoring of Coordinate class

--- test/test-coordinate.js
+++ test/test-coordinate.js
@@ -42,52 +42,128 @@ module.exports = testCase({
             var p1 = new GeoCoordinate([testData[0], testData[1]]);
             var p2 = new GeoCoordinate([testData[2], testData[3]]);
             equalDecimal(test, p1.distanceTo(p2), testData[4], testData[4] * 0.015, "too big deviation (distanceTo)");
-            equalDecimal(test, p1._quickDistanceTo(p2), testData[4], testData[4] * 0.015, "too big deviation (_quickDistanceTo)");
+            equalDecimal(test, p1.quickDistanceTo(p2), testData[4], testData[4] * 0.015, "too big deviation (quickDistanceTo)");
         }
         test.done();
     },
     "test get bearing north": function(test) {
-        var north = new GeoCoordinate([3, 0, 0]);
+        const north = new GeoCoordinate([3, 0, 0]);
         equalDecimal(test, P1.bearingTo(north), 0, 0, "north direction should be 0°");
         test.done();
     },
     "test get bearing northeast": function(test) {
-        var northeast = new GeoCoordinate([3, 3, 0]);
+        const northeast = new GeoCoordinate([3, 3, 0]);
         equalDecimal(test, P1.bearingTo(northeast), degMinSecToDec(44,57,39), maxDelta, "northeast not correctly calculated");
         test.done();
     },
     "test get bearing east": function(test) {
-        var east = new GeoCoordinate([0, 120, 0]);
+        const east = new GeoCoordinate([0, 120, 0]);
         equalDecimal(test, P1.bearingTo(east), degMinSecToDec(90,0,0), 0, "east not correctly calculated");
         test.done();
     },
     "test get bearing southeast": function(test) {
-        var southeast = new GeoCoordinate([-10, 10, 0]);
+        const southeast = new GeoCoordinate([-10, 10, 0]);
         equalDecimal(test, P1.bearingTo(southeast), degMinSecToDec(135,26,19), maxDelta, "southeast not correctly calculated");
         test.done();
     },
     "test get bearing southwest": function(test) {
-        var southwest = new GeoCoordinate([-45, -13, 0]);
+        const southwest = new GeoCoordinate([-45, -13, 0]);
         equalDecimal(test, P1.bearingTo(southwest), degMinSecToDec(192,40,40), maxDelta, "southwest not correctly calculated");
         test.strictEqual(P1.bearingRadTo(southwest), 3.3628605082691405, "rad north should be calculated too");
         test.done();
     },
     "test get bearing almost north": function(test) {
-        var north = new GeoCoordinate([80, -0.2, 0]);
+        const north = new GeoCoordinate([80, -0.2, 0]);
         equalDecimal(test, P1.bearingTo(north), degMinSecToDec(359,57,53), maxDelta, "almost north not correctly calculated");
         test.strictEqual(P1.bearingRadTo(north), 6.282569811232558, "rad north should be calculated too");
         test.done();
     },    
     "test get bearing almost north 2": function(test) {
-        var north = new GeoCoordinate([52.50, 13.00, 0]);
+        const north = new GeoCoordinate([52.50, 13.00, 0]);
         equalDecimal(test, P2.bearingTo(north), degMinSecToDec(0,0,0), maxDelta, "almost north not correctly calculated");
         test.strictEqual(P2.bearingRadTo(north), 0, "rad north should be calculated too");
         test.done();
     },
     "test get bearing northern hemisphers": function(test) {
-        var bagdad = new GeoCoordinate([35,45,120]);
-        var osaka = new GeoCoordinate([35,135,200]); 
+        const bagdad = new GeoCoordinate([35,45,120]);
+        const osaka = new GeoCoordinate([35,135,200]);
         equalDecimal(test, bagdad.bearingTo(osaka), degMinSecToDec(60,9,45), maxDelta, "bagdad osaka not correctly calculated");
+        test.done();
+    },
+    "test get altitude": function(test) {
+        const bagdad = new GeoCoordinate([35,45,120]);
+        equalDecimal(test, bagdad.altitude(), 120, 0, 'Bagdad altitude doesn\'t match');
+        const randomPoint = new GeoCoordinate([40,24]);
+        equalDecimal(test, randomPoint.altitude(), 0, 0, 'Default altitude isn\'t 0');
+        test.done();
+    },
+    "test input as arguments": function(test) {
+        test.throws(function() {
+            new GeoCoordinate(null, 2, 3);
+        });
+        test.throws(function() {
+            new GeoCoordinate(1);
+        });
+        test.doesNotThrow(function() {
+            new GeoCoordinate(5, 6);
+            new GeoCoordinate(7, 8, 9);
+        });
+        test.done();
+    },
+    "test input as array": function(test) {
+        test.throws(function() {
+            new GeoCoordinate([1]);
+        });
+        test.throws(function(){
+            new GeoCoordinate([null, 1]);
+        });
+
+        test.throws(function() {
+            new GeoCoordinate([1, 2, null]);
+        });
+
+        test.doesNotThrow(function () {
+            new GeoCoordinate([26, 42]);
+        });
+
+        test.done();
+    },
+    "test input as object": function(test) {
+        test.throws(function() {
+            new GeoCoordinate({});
+        });
+        test.throws(function(){
+            new GeoCoordinate({
+                latitude: 0
+            });
+        });
+
+        test.throws(function(){
+            new GeoCoordinate({
+                altitude: 0
+            });
+        });
+        test.throws(function(){
+            new GeoCoordinate({
+                latitude: 'wow',
+                altitude: 0
+            });
+        });
+
+        test.doesNotThrow(function () {
+            new GeoCoordinate({
+                latitude: 0,
+                longitude: 0,
+                altitude: 0
+            });
+        });
+
+        test.done();
+    },
+    "test constructor usable as factory": function(test) {
+        test.doesNotThrow(function() {
+            GeoCoordinate(1,1);
+        });
         test.done();
     }
 });
